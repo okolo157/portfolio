d4ccc59a349fc3db0c91a4062d97295525c21454
vik1234-del <victor.okolo@pau.edu.ng>
2025-02-10 03:42:04 +0100

    refactor Navbar and HomePage components for improved layout and content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="flex justify-center md:justify-between py-5 gap-4 sm:gap-6 md:gap-14 px-4 sm:px-8 md:px-14">
-      <div className="flex flex-col justify-center w-1/4">
+      <div className="flex flex-col justify-center w-1/3">
         <div className="flex justify-around hover:opacity-60 ease-in-out">
           <Link href="/home" className="text-sm sm:text-base">
-            HOME
+            MY STORY
           </Link>
           <p className="text-sm sm:text-base">•</p>
         </div>
         <hr className="my-2 text-white self-center w-[60px] sm:w-[70px] md:w-3/5 border" />
       </div>
-      <div className="flex flex-col justify-center w-1/4">
+      <div className="flex flex-col justify-center w-1/3">
         <div className="flex justify-around hover:opacity-60 ease-in-out">
           <Link href="/home" className="text-sm sm:text-base">
             WORK
@@ -21,7 +21,7 @@ export default function Navbar() {
         </div>
         <hr className="my-2 text-white self-center w-[60px] sm:w-[70px] md:w-3/5 border" />
       </div>
-      <div className="flex flex-col justify-center w-1/4">
+      {/* <div className="flex flex-col justify-center w-1/4">
         <div className="flex justify-around hover:opacity-60 ease-in-out">
           <Link href="/home" className="text-sm sm:text-base">
             ABOUT
@@ -29,8 +29,8 @@ export default function Navbar() {
           <p className="text-sm sm:text-base">•</p>
         </div>
         <hr className="my-2 text-white self-center w-[60px] sm:w-[70px] md:w-3/5 border" />
-      </div>
-      <div className="flex flex-col justify-center w-1/4">
+      </div> */}
+      <div className="flex flex-col justify-center w-1/3">
         <div className="flex justify-around hover:opacity-60 ease-in-out">
           <Link href="/home" className="text-sm sm:text-base">
             HMU
