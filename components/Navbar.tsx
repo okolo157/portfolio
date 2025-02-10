9225aee8d578690197750df5a9918b56bd3beaec
vik1234-del <victor.okolo@pau.edu.ng>
2025-02-10 22:21:45 +0100

    hot beans

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="flex justify-center md:justify-between py-5 gap-4 sm:gap-6 md:gap-14 px-4 sm:px-8 md:px-14">
       <div className="flex flex-col justify-center w-1/3">
         <div className="flex justify-around hover:opacity-60 ease-in-out cursor-cell">
-          <Link href="/home" className="text-sm sm:text-base cursor-cell">
+          <Link href="/" className="text-sm sm:text-base cursor-cell">
             VICTOR
           </Link>
           <p className="text-sm sm:text-base">•</p>
