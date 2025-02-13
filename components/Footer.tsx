cb2cb5c604f906fb5ec360efd659e6eca30f61f1
vik1234-del <victor.okolo@pau.edu.ng>
2025-02-13 21:33:06 +0100

    final?

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,35 +10,46 @@ const robotoMono = Roboto_Mono({
 
 export default function Footer() {
   return (
-    <div className={robotoMono.className}>
-      <div className="flex flex-col bg-black text-white text-xl sm:text-2xl md:text-3xl p-4 sm:p-6 md:p-10 items-center gap-4 sm:gap-5 md:gap-7">
+    <div
+      className={robotoMono.className + " bg-black/10 border-t border-white/10"}
+    >
+      {" "}
+      <div className="flex flex-col justify-center text-xl sm:text-2xl md:text-3xl p-4 sm:p-6 md:p-10 items-center gap-4 sm:gap-5 md:gap-7 max-w-7xl mx-auto">
         <div className="flex justify-center items-center gap-4 sm:gap-6 md:gap-10">
           <p>*</p>
           <p className="text-base sm:text-lg md:text-xl">Victor Okolo</p>
           <p>*</p>
         </div>
-        <div className="flex flex-wrap justify-center text-xs sm:text-sm md:text-base gap-2 sm:gap-3 md:gap-4 max-w-full px-2">
+        <div className="flex  justify-center text-xs sm:text-sm md:text-base gap-2 sm:gap-3 md:gap-4 max-w-full px-1">
           <Link
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://github.com/okolo157"
-            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-1 sm:p-2 m-1"
+            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-2 sm:p-3 m-1"
           >
             Github
           </Link>
           <Link
-            href="www.linkedin.com/in/victorokolo11"
-            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-1 sm:p-2 m-1"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://linkedin.com/in/victorokolo11"
+            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-2 sm:p-3 m-1"
           >
             LinkedIn
           </Link>
           <Link
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://wa.me/2349110591628"
-            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-1 sm:p-2 m-1"
+            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-2 sm:p-3 m-1"
           >
             Whatsapp
           </Link>
           <Link
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://flowcv.com/resume/ilbnbmi60q"
-            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-1 sm:p-2 m-1"
+            className="border text-white hover:bg-white hover:text-black transition-colors ease-in-out p-2 sm:p-3 m-1"
           >
             Resume
           </Link>
