5480d3bd8d6c1dc507352fe73a57776cc7c24ac8
Victor Okolo <64779006+okolo157@users.noreply.github.com>
2025-06-12 20:41:49 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["github-readme-stats.vercel.app", "i.imgur.com", "imgur.com"],
-    dangerouslyAllowSVG: true, // Allow SVGs
+    dangerouslyAllowSVG: true, 
     contentSecurityPolicy: "default-src 'self'; img-src * data: blob:;", // Add CSP for security
   },
 };
