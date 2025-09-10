286d0aa42e18f912a4d69d496394cd8ed017cdb2
Victor Okolo <64779006+okolo157@users.noreply.github.com>
2025-09-10 14:55:20 +0100

    Update Lottie.tsx

--- components/Lottie.tsx
+++ components/Lottie.tsx
@@ -1,26 +1,24 @@
 "use client";
-
 import { useEffect, useRef } from "react";
 import { DotLottie } from "@lottiefiles/dotlottie-web";
 
 export default function Lottie() {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    if (!containerRef.current) return;
+    if (!canvasRef.current) return;
 
     const player = new DotLottie({
+      canvas: canvasRef.current, 
       autoplay: true,
       loop: true,
       src: "https://lottie.host/6b773300-4158-41c3-9d74-579efa66da8a/RXD3LRsYf6.lottie",
     });
-
-    player.mount(containerRef.current);
 
     return () => {
       player.destroy();
     };
   }, []);
 
-  return <div ref={containerRef} className="w-[120px] h-[120px]" />;
+  return <canvas ref={canvasRef} className="w-[120px] h-[120px]" />;
 }
