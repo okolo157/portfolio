bab77da5fc4dfad3fad607c6f8c50c5df6eae4d5
Victor Okolo <64779006+okolo157@users.noreply.github.com>
2025-09-10 14:44:00 +0100

    Create lottie.tsx

---
+++ components/lottie.tsx
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'; 
+
+const App = () => { 
+  return ( 
+    <DotLottieReact src="https://lottie.host/6b773300-4158-41c3-9d74-579efa66da8a/RXD3LRsYf6.lottie" loop autoplay /> 
+  ); 
+};
