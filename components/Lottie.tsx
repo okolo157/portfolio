dd1c7b2bb08be0b0a9a6d3e59181e464ee741ae6
Victor Okolo <64779006+okolo157@users.noreply.github.com>
2025-09-15 15:08:57 +0100

    Update Lottie.tsx

--- components/Lottie.tsx
+++ components/Lottie.tsx
@@ -46,7 +46,7 @@ export function LottieCamera() {
   }, []);
 
   return (
-    <div className="absolute top-2 right-1">
+    <div className="absolute right-1">
       <canvas ref={canvasRef} className="w-[120px] h-[120px]" />
     </div>
   );
