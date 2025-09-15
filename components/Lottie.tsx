fc63586428a27e988df415101a5a6ca7d0bc390f
Victor Okolo <64779006+okolo157@users.noreply.github.com>
2025-09-15 14:50:43 +0100

    Update Lottie.tsx

--- components/Lottie.tsx
+++ components/Lottie.tsx
@@ -26,3 +26,28 @@ export default function Lottie() {
     </div>
   );
 }
+
+export default function LottieCamera() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+   useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const player = new DotLottie({
+      canvas: canvasRef.current,
+      autoplay: true,
+      loop: true,
+      src: "https://lottie.host/47d07f91-a1c4-4664-b489-3dbcf2961ded/F9olLHMA36.lottie",
+    });
+
+    return () => {
+      player.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="absolute top-2 right-1">
+      <canvas ref={canvasRef} className="w-[120px] h-[120px]" />
+    </div>
+  );
+}
